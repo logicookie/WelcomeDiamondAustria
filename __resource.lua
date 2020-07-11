3fa1e3d57091e5725686cfd689cee14b4c1c0c41
Adrian | LogiCookie <68113413+logicookie@users.noreply.github.com>
2020-07-11 19:36:11 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'LogiCookie'
+description 'DiamondAustriaWelcome'
+version '1.0.0'
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+client_script "va_client.lua"
+
---
+++ client.lua
@@ -0,0 +1,4 @@
+AddEventHandler('playerSpawned', function(spawn)
+    TriggerEvent('chatMessage', 'SYSTEM', {255, 255, 255}, "Willkommen auf dem Diamond Austria FiveM Server!")
+    TriggerEvent('chatMessage', 'SYSTEM', {255, 255, 255}, "Bitte benehme dich und halte dich an die Regeln!")
+    end)
